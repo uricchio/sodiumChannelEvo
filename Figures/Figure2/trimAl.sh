fe7a75090c444d69d7c1888ed93f7fd09b5b2981
Lawrence Uricchio <uricchio@telemacher.local>
2022-08-10 09:59:46 -0700

    updated Fig 2

--- Figures/Figure2/trimAl.sh
+++ Figures/Figure2/trimAl.sh
@@ -1,1 +1,1 @@
-~/projects/software/trimAl/source/trimal -in $1 -out ./plot_data/$2.fa -resoverlap 0.80 -seqoverlap 80
+~/projects/software/trimAl/source/trimal -in $1 -out ./plot_data/$2.fa -resoverlap 0.81 -seqoverlap 80
